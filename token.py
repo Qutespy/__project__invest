945cfe3e2d7dba3100b64d15a69879d6a6cc62bc
0Irina1Rudneva28 <104107798+0Irina1Rudneva28@users.noreply.github.com>
2022-05-29 14:12:09 +0300

    Create token.py

---
+++ token.py
@@ -0,0 +1,3 @@
+class My_tokens:
+    token_to_read_brokerage_account = 't.a2P9hzNe28P5ZPbC1eT8uIYv6Zy5YkjXvpqvuQZZwFJCXms1_NVxWmLtiDTs8h7mhPzh-j388EHF7SF6nl5qbQ'
+    token_to_read_iic = 't.K-gHkYEHPBI-djaCXn8gKTdmzBdqs_jqz4aHLQjWuJRjkoV4k6iLvQ7QXuH8-fEoe6S6QLCFTjZ6YcG6u6wdxw'
