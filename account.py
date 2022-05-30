138b793b12bec40a99e45ae9d9483dc3eccfcb2f
0Irina1Rudneva28 <104107798+0Irina1Rudneva28@users.noreply.github.com>
2022-05-30 18:36:51 +0300

    Update account.py

--- account.py
+++ account.py
@@ -17,7 +17,7 @@ class USER:
         self.client = client
         self.__accounts = []
         self.__position_wallet = []
-        self.operation = []
+        self.__operation = []
         self.__wallet = []
         self.with_draw_limitsResponse=[]
 
@@ -120,7 +120,182 @@ class USER:
         '''возвращает число во float'''
         return val.units + val.nano / 1e9
 
+    def __PrintingOperationState(self, name):
+        if name == 'OPERATION_STATE_UNSPECIFIED':
+            str = "Статус операции не определён"
+            return str
+        elif name == 'OPERATION_STATE_EXECUTED':
+            str = "Исполнена"
+            return str
+        elif name == 'OPERATION_STATE_CANCELED':
+            str = "Отменена"
+            return str
 
+    def __PrintingOperationType(self, name):
+        if name == 'OPERATION_TYPE_UNSPECIFIED':
+            str = "Тип операции не определён"
+            return str
+        elif name == 'OPERATION_TYPE_INPUT':
+            str = "Пополнение брокерского счёта"
+            return str
+        elif name == 'OPERATION_TYPE_BOND_TAX':
+            str = "Удержание НДФЛ по купонам"
+            return str
+        elif name == 'OPERATION_TYPE_OUTPUT_SECURITIES':
+            str = "Вывод ЦБ"
+            return str
+        elif name == 'OPERATION_TYPE_OVERNIGHT':
+            str = "Доход по сделке РЕПО овернайт"
+            return str
+        elif name == 'OPERATION_TYPE_TAX':
+            str = "Удержание налога"
+            return str
+        elif name == 'OPERATION_TYPE_BOND_REPAYMENT_FULL':
+            str = "Полное погашение облигаций"
+            return str
+        elif name == 'OPERATION_TYPE_SELL_CARD':
+            str = "Продажа ЦБ с карты"
+            return str
+        elif name == 'OPERATION_TYPE_DIVIDEND_TAX':
+            str = "Удержание налога по дивидендам"
+            return str
+        elif name == 'OPERATION_TYPE_OUTPUT':
+            str = "Вывод денежных средств"
+            return str
+        elif name == 'OPERATION_TYPE_BOND_REPAYMENT':
+            str = "Частичное погашение облигаций"
+            return str
+        elif name == 'OPERATION_TYPE_TAX_CORRECTION':
+            str = "Корректировка налога"
+            return str
+        elif name == 'OPERATION_TYPE_SERVICE_FEE':
+            str = "Удержание комиссии за обслуживание брокерского счёта"
+            return str
+        elif name == 'OPERATION_TYPE_BENEFIT_TAX':
+            str = "Удержание налога за материальную выгоду"
+            return str
+        elif name == 'OPERATION_TYPE_MARGIN_FEE':
+            str = "Удержание комиссии за непокрытую позицию"
+            return str
+        elif name == 'OPERATION_TYPE_BUY':
+            str = "Покупка ЦБ"
+            return str
+        elif name == 'OPERATION_TYPE_BUY_CARD':
+            str = "Покупка ЦБ с карты"
+            return str
+        elif name == 'OPERATION_TYPE_INPUT_SECURITIES':
+            str = "Перевод ценных бумаг из другого депозитария"
+            return str
+        elif name == 'OPERATION_TYPE_SELL_MARGIN':
+            str = "Продажа в результате Margin-call"
+            return str
+        elif name == 'OPERATION_TYPE_BROKER_FEE':
+            str = "Удержание комиссии за операцию"
+            return str
+        elif name == 'OPERATION_TYPE_BUY_MARGIN':
+            str = "Покупка в результате Margin-call"
+            return str
+        elif name == 'OPERATION_TYPE_DIVIDEND':
+            str = "Выплата дивидендов"
+            return str
+        elif name == 'OPERATION_TYPE_SELL':
+            str = "Продажа ЦБ"
+            return str
+        elif name == 'OPERATION_TYPE_COUPON':
+            str = "Выплата купонов"
+            return str
+        elif name == 'OPERATION_TYPE_SUCCESS_FEE':
+            str = "Удержание комиссии SuccessFee"
+            return str
+        elif name == 'OPERATION_TYPE_DIVIDEND_TRANSFER':
+            str = "Передача дивидендного дохода"
+            return str
+        elif name == 'OPERATION_TYPE_ACCRUING_VARMARGIN':
+            str = "Зачисление вариационной маржи"
+            return str
+        elif name == 'OPERATION_TYPE_WRITING_OFF_VARMARGIN':
+            str = "Списание вариационной маржи"
+            return str
+        elif name == 'OPERATION_TYPE_DELIVERY_BUY':
+            str = "Покупка в рамках экспирации фьючерсного контракта"
+            return str
+        elif name == 'OPERATION_TYPE_DELIVERY_SELL':
+            str = "Продажа в рамках экспирации фьючерсного контракта"
+            return str
+        elif name == 'OPERATION_TYPE_TRACK_MFEE':
+            str = "Комиссия за управление по счёту автоследования"
+            return str
+        elif name == 'OPERATION_TYPE_TRACK_PFEE':
+            str = "Комиссия за результат по счёту автоследования"
+            return str
+        elif name == 'OPERATION_TYPE_TAX_PROGRESSIVE':
+            str = "Удержание налога по ставке 15%"
+            return str
+        elif name == 'OPERATION_TYPE_BOND_TAX_PROGRESSIVE':
+            str = 'Удержание налога по купонам по ставке 15%'
+            return str
+        elif name == 'OOPERATION_TYPE_DIVIDEND_TAX_PROGRESSIVE':
+            str = "Удержание налога по дивидендам по ставке 15%"
+            return str
+        elif name == 'OPERATION_TYPE_BENEFIT_TAX_PROGRESSIVE':
+            str = "Удержание налога за материальную выгоду по ставке 15%"
+            return str
+        elif name == 'OPERATION_TYPE_TAX_CORRECTION_PROGRESSIVE':
+            str = "Корректировка налога по ставке 15%"
+            return str
+        elif name == 'OPERATION_TYPE_TAX_REPO_PROGRESSIVE':
+            str = "Удержание налога за возмещение по сделкам РЕПО по ставке 15%"
+            return str
+        elif name == 'OPERATION_TYPE_TAX_REPO':
+            str = "Удержание налога за возмещение по сделкам РЕПО"
+            return str
+        elif name == 'OPERATION_TYPE_TAX_REPO_HOLD':
+            str = "Удержание налога по сделкам РЕПО"
+            return str
+        elif name == 'OPERATION_TYPE_TAX_REPO_REFUND':
+            str = "Возврат налога по сделкам РЕПО"
+            return str
+        elif name == 'OPERATION_TYPE_TAX_REPO_HOLD_PROGRESSIVE':
+            str = "Удержание налога по сделкам РЕПО по ставке 15%"
+            return str
+        elif name == 'OPERATION_TYPE_TAX_REPO_REFUND_PROGRESSIVE':
+            str = "Возврат налога по сделкам РЕПО по ставке 15%"
+            return str
+        elif name == 'OPERATION_TYPE_DIV_EXT':
+            str = "Выплата дивидендов на карту"
+            return str
+        elif name == 'OPERATION_TYPE_TAX_CORRECTION_COUPON':
+            str = "	Корректировка налога по купонам"
+            return str
+
+    def get_operations(self, from__, to__):
+        '''
+             возвращает все транзакции
+                                          '''
+        for acc_id in self.return_account_id():
+            for operation in self.client.operations.get_operations(
+                    account_id=acc_id,
+                    from_=datetime.datetime(*from__),
+                    to=datetime.datetime(*to__)
+            ).operations:
+                self.__operation.append([operation.id, operation.currency, self.__cast_money(operation.payment),
+                                       self.__cast_money(operation.price), self.__PrintingOperationState(operation.state.name),
+                                       operation.quantity,operation.quantity_rest, operation.figi,
+                                       operation.instrument_type,operation.date,
+                                       self.__PrintingOperationType(operation.operation_type.name)])
+
+            all_operation_user = pd.DataFrame(data=self.__operation,
+                                            columns=['id', 'Валюта операции', 'Сумма операции',
+                                                     'Цена операции за 1 инструмент', 'Статус операции',
+                                                     'Количество единиц инструмента', 'Неисполненный остаток по сделке',
+                                                     'Figi-идентификатор инструмента', 'Тип инструмента',
+                                                     'Дата и время операции в формате часовом поясе UTC',
+                                                     'Тип операции'],
+                                              index=range(1, len(self.__operation) + 1)
+                                              )
+        all_operation_user.to_csv('operation.csv')
+        return all_operation_user
+    
 with Client(My_tokens.token_main_ro_all_accounts) as client:
     data = USER(client).get_portfolio_position()
     print(data)
